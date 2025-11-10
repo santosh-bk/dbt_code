8599f2147541b54d8ad6fc6e2038f7b3c0ec5f1f
Santosh K <sk033b@gmail.com>
2025-11-10 07:55:14 +0000

    commit

---
+++ snapshots/snap_tags.sql
@@ -0,0 +1,11 @@
+{% snapshot snap_tags %}
+
+{{
+    config(
+        target_schema='snapshots',
+        unique_key=['user_id','movie_id','tag'],
+        strategy='timestamp',
+        updated_at='tag_timestamp',
+        invalidate_hard_deletes=True
+    )
+}}
